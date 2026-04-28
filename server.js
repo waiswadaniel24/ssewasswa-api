1f2a9dbc7de7267a4b0a826d92a43c16488c4e9a
ssewasswa <waiswadaniel24@email.com>
2026-04-28 14:23:57 +0300

    Add fixed reset-admin route

--- server.js
+++ server.js
@@ -404,3 +404,4 @@ app.get('/reset-admin', async (req, res) => {
 });
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
