e322aa63576c470fe6f2b438024bdaf66338ba09
waiswadaniel24 <waiswadaniel24@gmail.com>
2026-05-04 23:29:49 +0300

    Update server.js

--- server.js
+++ server.js
@@ -32,11 +32,10 @@ app.use(session({
   resave: false,
   saveUninitialized: false,
   cookie: {
-  secure: false, // Change this from true to false
-  httpOnly: true,
-  maxAge: 24 * 60 * 60 * 1000,
-  sameSite: 'lax'
-}
+    secure: false,
+    httpOnly: true,
+    maxAge: 24 * 60 * 60 * 1000,
+    sameSite: 'lax'
   }
 }));
 // === MTN MOMO CONFIG - ADD YOUR KEYS TO RENDER ENV ===
