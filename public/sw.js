2c7bfad408b6d5bd7ba4d33b590e736cfab89458
waiswadaniel24 <waiswadaniel24@gmail.com>
2026-05-09 13:19:46 +0300

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,6 @@
-const CACHE='ssewasswa-v6.0';
-const urlsToCache=['/','/offline','/manifest.json','/icon-192.png'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(urlsToCache))));
-self.addEventListener('fetch',e=>{if(e.request.method==='GET')e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).catch(()=>caches.match('/offline'))));});
-self.addEventListener('sync',e=>{if(e.tag==='sync-data')e.waitUntil(syncOfflineData());});
-async function syncOfflineData(){const db=await indexedDB.open('ssewasswa',1);}
+const CACHE = 'ssewasswa-v9.0';
+const urlsToCache = ['/', '/offline', '/manifest.json'];
+self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(urlsToCache))));
+self.addEventListener('fetch', e => {
+  if (e.request.method === 'GET') e.respondWith(caches.match(e.request).then(r => r || fetch(e.request).catch(() => caches.match('/offline'))));
+});
